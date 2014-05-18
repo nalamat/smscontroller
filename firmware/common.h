b36b3368a0fa3f777b0d032e321fb9a127b4e418
Nima Alamatsaz <nnalamat@gmail.com>
2014-05-18 14:46:35 +0430

    Removed inline from delay_us and delay_ms

--- firmware/common.h
+++ firmware/common.h
@@ -34,7 +34,7 @@
 
 void *operator new(size_t objsize);
 void operator delete(void* obj);
-void inline delay_us(unsigned long us);
-void inline delay_ms(unsigned long ms);
+void delay_us(unsigned long us);
+void delay_ms(unsigned long ms);
 
 #endif
